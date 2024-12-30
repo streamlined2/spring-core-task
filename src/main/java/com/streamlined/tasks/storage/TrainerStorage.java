a2ff1ece89170dc95f756e2db9fe9f3922321d30
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-12-30 15:46:46 +0200

    Fixed auto-wiring error of TrainerStorage bean
    1. TrainerStorage class constructor with TrainerParser dependency marked
    for auto-wiring.

--- src/main/java/com/streamlined/tasks/storage/TrainerStorage.java
+++ src/main/java/com/streamlined/tasks/storage/TrainerStorage.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.streamlined.tasks.entity.Trainer;
@@ -21,6 +22,7 @@ public class TrainerStorage {
 		trainerMap = new HashMap<>();
 	}
 
+	@Autowired
 	public TrainerStorage(TrainerParser trainerParser) {
 		this();
 		this.trainerParser = trainerParser;
